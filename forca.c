2e7dc778dd731da81b59e6224c1dc07e0e6b3654
ArthurOReis <arthuroreis2005@gmail.com>
2024-10-06 15:10:02 -0300

    Criando funções para diminuir main()

--- forca.c
+++ forca.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+void abertura() {
+    printf("*************************\n");
+    printf("*     Jogo da forca     *\n");
+    printf("*************************\n\n");
+}
+
+void chuta(char chutes[], int *tentativas){
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[(*tentativas)] = chute;
+    (*tentativas)++;
+
+}
+
 int main(){
     char palavrasecreta[20];
 
@@ -12,18 +27,21 @@ int main(){
     char chutes[26];
     int tentativas = 0;
 
+    abertura();
+
     do {
 
         for (int i = 0; i < strlen(palavrasecreta); i++){
 
             int achou = 0;
-            for (int j = 0; j < tentativas; j++){
+
+            for (int j = 0; j < tentativas; j++){ // Será procurado na lista dos chutes, um caractere que corresponda ao da palavra secreta, caso 'true', aparecerá a letra entre os espaços
                 if(chutes[j] == palavrasecreta[i]){
                     achou = 1;
                     break;
                 }
             }
-            
+
             if (achou){
                 printf("%c ", palavrasecreta[i]);
             } else {
@@ -32,12 +50,8 @@ int main(){
         }
 
         printf("\n\n");
-        
-        char chute;
-        scanf(" %c", &chute);
 
-        chutes[tentativas] = chute;
-        tentativas++;
+        chuta(chutes, &tentativas);
 
     } while (!acertou && !enforcou);
 }
